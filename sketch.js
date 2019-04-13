155bcb49a378d1cb45334cb531b3c813dc092106
liwszu <35881053+liwszu@users.noreply.github.com>
2019-04-13 00:34:11 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(710, 400);
-   body = new Body(width/2, height/2, 100, 100);
+  this.body = new Body(width/2, height/2, 100, 100);
 }
 
 function draw() {
@@ -9,6 +9,8 @@ function draw() {
   ellipse(width - 600, height/8, 100, 100);
   fill(205,192,176);
   rect(0 , height/1.65, width*2, height/3);
+  this.body.display();
+  
 }
 
 
@@ -18,41 +20,32 @@ let Body = function(x,y,w,h) {
   this.y = y;
   this.w = w;
   this.h = h;
-  this.c = c;
-  this.xpos = xpos;
-  this.ypos = ypos;
+  
+  
 }
-
 Body.prototype.display = function() {
-  if(x > width){
-    x = 5; //keep human on page
+  
+    this.x = 5; //keep human on page
     ellipseMode(CENTER);
     rectMode(CENTER);
     //torso
     stroke(0);
     fill(0);
-    rect(x,y,w/3,h/2);
+    rect(this.x,this.y,this.w/3,this.h/2);
     // head
     stroke(0);
     fill(0);
-    ellipse(x,y-h/1.8,w/3,h/2);
+    ellipse(this.x,this.y-this.h/1.8,this.w/3,this.h/2);
     // legs
     stroke(0);
-    line(x - w/8,y + h/3,x - w/4,y + h/2 + 8);
-    line(x + w/8,y + h/3,x + w/4,y + h/2 + 8);
+    line(this.x - this.w/8,this.y + this.h/3,this.x - this.w/4,this.y + this.h/2 + 8);
+    line(this.x + this.w/8,this.y + this.h/3,this.x + this.w/4,this.y + this.h/2 + 8);
     // arms
     stroke(0);
-    line(x - w/3, y - h/2, x-w/6, y - h/8);
-    line(x + w/3, y - h/2, x+w/6, y - h/8);
+    line(this.x - this.w/3, this.y - this.h/2, this.x-this.w/6, this.y - this.h/8);
+    line(this.x + this.w/3, this.y - this.h/2, this.x+this.w/6, this.y - this.h/8);
+  
 
-
-  }
-
-Body.prototype.walk = function() {
-    x = x + 1;
-    if (x > width) {
-      x = 0;
-    }
-  }
+}
 
 
